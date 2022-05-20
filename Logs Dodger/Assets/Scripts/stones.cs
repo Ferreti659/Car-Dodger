9b376e8f88f88b2a5eb850d42d1c26b09b541838
Pablo Jiménez Rodríguez <pablo.jimenez.external@atos.net>
2022-05-20 02:16:44 +0200

    ya caen rocas aleatoriamente

--- Logs Dodger/Assets/Scripts/stones.cs
+++ Logs Dodger/Assets/Scripts/stones.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class stones : MonoBehaviour
-{
+public class stones : MonoBehaviour { 
+
+    float speed = 7;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
     }
 }
